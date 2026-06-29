343fc03b22cb2f603bcb47cab7ecdae830d34edd
Adrienne Lee <adrlee@uchicago.edu>
2026-06-28 19:25:46 -0500

    Potential fix to disappearing hamburger button on iPhone; changed events divs to avoid overflow

--- scripts-navbar.js
+++ scripts-navbar.js
@@ -2,19 +2,25 @@
 /**need a function so that when in column navbar form, navbar is shortened/divs display none when menu not clicked */
 button = document.getElementById("hamburger");
 menu = document.getElementById("menu-options");
+var windowWidth;
 
 function changeNavbar() {
-  if (window.innerWidth >= 350) {
+  if (window.innerWidth != windowWidth && window.innerWidth >= 480) {
     menu.style.display = `flex`;
     button.style.display = `none`;
-  } else {
+  } else if (window.innerWidth != windowWidth && window.innerWidth < 480) {
     button.style.display = `flex`;
     menu.style.display = `none`;
   }
+  windowWidth = window.innerWidth;
 }
 
 //resize, navbar
 window.addEventListener("resize", changeNavbar);
+
+window.addEventListener("load", () => {
+    windowWidth = window.innerWidth;
+});
 
 //click hamburger button (if collapsed)
 button.addEventListener("click", () => {
