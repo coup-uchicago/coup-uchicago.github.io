f0f719e72d1ef91a64aade2ab963e53a6f377a5d
Adrienne Lee <adrlee@uchicago.edu>
2026-07-27 14:40:55 -0500

    Edited navbar so that dropdowns work on touchscreen devices

--- scripts-navbar.js
+++ scripts-navbar.js
@@ -15,7 +15,7 @@ function changeNavbar() {
   windowWidth = window.innerWidth;
 }
 
-//resize, navbar
+//resize navbar
 window.addEventListener("resize", changeNavbar);
 
 window.addEventListener("load", () => {
@@ -30,3 +30,36 @@ button.addEventListener("click", () => {
     menu.style.display = "flex";
   }
 });
+
+//if "events" clicked, opens dropdown (for devices with no hover)
+canHover = window.matchMedia("(hover : hover)");
+if(!canHover.matches){
+    const navItems = document.querySelectorAll(".nav-desktop");
+    navItems.forEach((navItem) => {
+        if (navItem.children.length > 1){   //target only dropdowns
+            navItem.addEventListener("click", (event) => {
+                var li = event.target;  //the selected li
+                if (li.tagName == "SPAN"){
+                    li = li.parentElement;
+                }
+
+                const children = li.querySelectorAll(".below");
+
+                //close dropdown
+                if (children[0].style.display == "block"){
+                    children.forEach((child) => {
+                        child.style.display = "none";  
+                    });
+                    li.style.backgroundColor = "#fe9a20";
+                    li.style.zIndex = "auto";
+                } else {    //open dropdown
+                    children.forEach((child) => {
+                        child.style.display = "block";  
+                    });
+                    li.style.backgroundColor = "#990f4b";
+                    li.style.zIndex = "90";   
+                }
+            });
+        }
+    });
+}
