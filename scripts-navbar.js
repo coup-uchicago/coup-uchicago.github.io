6bd28e5fea9347c877c75cdb27e1ffcfefd9b7e9
Adrienne Lee <adrlee@uchicago.edu>
2026-07-27 21:39:56 -0500

    Added (untested) code to collapse previously opened dropdowns when a new dropdown is opened; adjusted the font size of h1 and the events divs

--- scripts-navbar.js
+++ scripts-navbar.js
@@ -59,12 +59,25 @@ if(!canHover.matches){
                 if (li.tagName == "SPAN"){
                     li = li.parentElement;
                 }
-                console.log(li);
-                
+                //console.log(li);
+
                 //toggle dropdown
                 if(li.classList.contains("open")){
                     li.classList.remove("open");
                 } else {
+                    //close previously opened dropdowns (untested)
+                    const navItems = document.querySelectorAll(".nav-item");
+                    navItems.forEach((navItem) => {
+                        if (navItem.classList.contains("open")){
+                            navItem.style.backgroundColor = orange;
+                            navItem.style.zIndex = "auto";
+                            navItem.classList.remove("open");
+                        }
+                    });
+                    const activeDropdown = document.querySelectorAll(".dropdown-active");
+                    activeDropdown.forEach((item) => {
+                        item.remove();
+                    });
                     li.classList.add("open");
                 }
 
